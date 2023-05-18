d35f60b64a82f2423df26e74162388b63e764e60
eycha <silvlexonst@gmail.com>
2023-05-18 11:00:59 +0900

    신고button function adding

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
+  <div class="menu">
+    <a v-for="작명 in 메뉴들" :key="작명">{{ 작명 }}</a>
+  </div>
   <img alt="Vue logo" src="./assets/logo.png">
   <div>
     <h4>{{ products[0] }}</h4>
     <p>50 만원</p>
+    <button @click="increase">허위매물신고</button> <span>신고수 : {{ 신고수 }}</span>
   </div>
   <div>
-    <h4>{{products[1]}}</h4>
-    <p>30 만원</p>
+    <h4>{{ products[1] }}</h4>
+    <p>60 만원</p>
   </div>
   <div>
-    <h4>{{products[2]}}</h4>
-    <p>30 만원</p>
-  </div>  
+    <h4>{{ products[2] }}</h4>
+    <p>70 만원</p>
+  </div> 
 </template>
 
 <script>
@@ -21,8 +25,14 @@ export default {
   name: 'App',
   data() {
     return {
-      
+      신고수 : 0,
+      메뉴들 : ['Home', 'Shop', 'About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+    }
+  },
+  methods : {
+    increase() {
+      this.신고수 += 1;
     }
   },
   components: {
@@ -38,6 +48,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color : white;
+  padding: 10px;
+
 }
 </style>
