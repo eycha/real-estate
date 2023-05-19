35315dd6f002723fd64f4130dd02cef267145ed1
eycha <silvlexonst@gmail.com>
2023-05-19 14:59:35 +0900

    alert adding

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -3,7 +3,7 @@
     <div class="white-bg">
       <h4>{{ rooms[clicked].title}}</h4>
       <p>{{ rooms[clicked].content}}</p>
-      <input v-model="month">
+      <input v-model.number=month>
       <p>{{month}}개월 선택함 : {{rooms[clicked].price * month}}원</p>
       <button @click="close()">X</button>
     </div>
@@ -17,9 +17,20 @@ export default {
     data() {
       return {
         month :1,
-
       }
     },
+    watch : {
+      month(a) {
+        if (a>12) {
+          alert('13이상 입력하지 마셈');
+        }
+        if (typeof a != 'number') {
+          alert('숫자로만 쓰셈');
+          this.month = 1;
+        }
+      }
+    },
+
     props : {
       rooms : Object,
       clicked : Number,
@@ -28,7 +39,7 @@ export default {
     methods : {
       
       close() {
-        this.$emit('closeModal',false)
+        this.$emit('closeModal',false);
       }
 
     }
